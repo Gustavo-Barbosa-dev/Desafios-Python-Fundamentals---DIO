196e779dc699359e3693a45e5a41113a248b6f7c
Gustavo Barbosa <gustavo.b.antonelli@gmail.com>
2024-11-16 03:49:35 -0300

    Update verificacao_ano_bissexto.py

--- desafios de código/verificacao_ano_bissexto.py
+++ desafios de código/verificacao_ano_bissexto.py
@@ -2,9 +2,8 @@ def verificador_ano_bissexto(ano):
     if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
         return "SIM"
     else:
-        return "NÃO"
-   
+        return "NÃO"  
+        
 ano = int(input("Digite um ano: "))
-
 resultado = verificador_ano_bissexto(ano)
 print(resultado) 
