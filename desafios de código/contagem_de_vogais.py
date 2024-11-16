f1a04b6f897f8b4c0b4c182bedb46af6d6caf1ad
Gustavo Barbosa <gustavo.b.antonelli@gmail.com>
2024-11-16 03:51:35 -0300

    Update contagem_de_vogais.py

--- desafios de código/contagem_de_vogais.py
+++ desafios de código/contagem_de_vogais.py
@@ -1,17 +1,10 @@
 def conta_vogais(texto):
-    #  Defina um conjunto de vogais tanto minúsculas quanto maiúsculas:
     vogais = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-    #  Defina um contador para contar as vogais:
     contador = 0
-    #  Percorra o texto e verifique se cada caractere é uma vogal:
     for caractere in texto:
         if caractere in vogais:
             contador += 1
-    #  Retorne o contador:
     return contador
-# Solicitamos ao usuário que insira uma string:
 texto = input("Digite uma string: ")        
-#  Chame a função para contar as vogais:
 resultado = conta_vogais(texto)
-#  Imprima o resultado:
 print(f"O número de vogais na string '{texto}' é: {resultado}")
